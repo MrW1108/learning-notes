ca803108cfeb582ab9443ee33cfcb206ca9ebb2b
MrW1108 <1637011263@qq.com>
2021-10-08 23:18:14 +0800

    commit

---
+++ 算法/返回数组排序后原序号.js
@@ -0,0 +1,16 @@
+/**
+ * 对于一个整数数组arr,将数组中的每个元素替换为它们排序(从小到大）后的序号,如果两个元素相等，那么它们的序号相同。  
+ * @param {Array} arr 
+ * @returns Array
+ */
+function answer(arr) {
+    console.log(arr)
+    const res = [...new Set(arr)].sort((a, b) => a - b)
+    console.log(res)
+    return arr.map(el => {
+        console.log(res.indexOf(el))
+        return res.indexOf(el)
+    })
+}
+
+console.log(answer([4, 12, 9, 22, 8]))
