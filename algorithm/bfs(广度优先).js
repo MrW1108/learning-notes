0e138ccb9e273f0bb9e09399da4e6a9cc7afea58
MrW1108 <1637011263@qq.com>
2025-07-13 14:16:53 +0800

    fix: bfs

--- algorithm/bfs(广度优先).js
+++ algorithm/bfs(广度优先).js
@@ -6,17 +6,42 @@
  *    / \
  *   4   5
  *
- *   1 → 2 → 4 → 5 → 3
+ *   1 → 2 →3 → 4 → 5
  */
 
 const bfs = (root) => {
+  if (!root) return [];
   const queue = [root];
   const res = [];
   while (queue.length) {
     const node = queue.shift();
     res.push(node.val);
-    if (node.left) queue.push(left);
-    if (node.right) queue.push(right);
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
   }
   return res;
 };
+
+const tree = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 5,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: 3,
+    left: null,
+    right: null,
+  },
+};
+
+console.log(bfs(tree));
